efca48409552146d960c1c083c0b977154b6eff4
DIR\igor.gralak <igor.gralak>
2022-03-25 10:24:02 +0100

    -updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gsm
+
+First of all paste it to cmd
+
+docker run --name gymdb -e POSTGRES_PASSWORD=gymDB -d -p 5432:5432 postgres
--- gms/frontend/gymSystem/src/app/model/user_model.ts
+++ gms/frontend/gymSystem/src/app/model/user_model.ts
@@ -2,6 +2,10 @@ export class User{
   public id: number;
   public userId: string;
   public firstName: string;
+  public username: string;
+  public email: string;
+  public lastLoginDate: string;
+
 
 
 
